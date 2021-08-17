690c05b0a0b9a2e14e4f049e9056fa2dd33a8557
devferx <ferkki@hotmail.com>
2021-08-17 14:41:12 -0400

    :tada: [ADD] initial files

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+
+const StyledSection = styled.section`
+  color: hotpink;
+`;
+
+function HomePage() {
+  return <StyledSection>Welcome to Next.js!</StyledSection>;
+}
+
+export default HomePage;
