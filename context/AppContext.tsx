8e1b0e8ead85deaced51a15aa2bc678a3a55c42d
devferx <ferkki@hotmail.com>
2021-09-06 19:01:04 -0400

    :construction: [ADD] AppContext and react-hot-toast

---
+++ context/AppContext.tsx
@@ -0,0 +1,46 @@
+import { createContext, useEffect, useState } from "react";
+import { IProduct } from "../interfaces/product";
+
+interface IAppContext {
+  card: IProduct[];
+  isBrowser: boolean;
+  isProductModalOpen: boolean;
+  addToCard: (product: IProduct) => void;
+  removeFromCard: (productId: string) => void;
+  clearCard: () => void;
+}
+
+export const AppContext = createContext({} as IAppContext);
+
+interface AppContextProviderProps {
+  children: React.ReactNode;
+}
+
+export const AppContextProvider = ({ children }: AppContextProviderProps) => {
+  const [card, setcard] = useState([]);
+  const [isProductModalOpen, setIsProductModalOpen] = useState(false);
+  const [isBrowser, setIsBrowser] = useState(false);
+
+  useEffect(() => {
+    setIsBrowser(true);
+  }, []);
+
+  const addToCard = (product: IProduct) => {};
+  const removeFromCard = (productId: string) => {};
+  const clearCard = () => {};
+
+  return (
+    <AppContext.Provider
+      value={{
+        card,
+        isProductModalOpen,
+        isBrowser,
+        addToCard,
+        removeFromCard,
+        clearCard,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
-// import App from "next/app";
+import { Toaster } from "react-hot-toast";
 import type { AppProps } from "next/app";
+
+import { AppContextProvider } from "../context/AppContext";
 import { GlobalStyles } from "../styles/globalStyles";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <AppContextProvider>
+      <Toaster />
       <GlobalStyles />
       <Component {...pageProps} />
-    </>
+    </AppContextProvider>
   );
 }
 
